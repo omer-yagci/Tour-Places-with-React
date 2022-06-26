a40e5c54e7a556f72ff584bf46859aded83fc615
khan <omeryagci3295@gmail.com>
2022-06-26 11:11:17 +0300

    header added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "../../scss/header.module.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={classes.title}>POPULAR TOUR PLACES</h1>
+    </div>
+  );
+};
+
+export default Header;
